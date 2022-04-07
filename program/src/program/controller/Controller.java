f1a12f9cb0481bd5816d525aa2397e8b88cb578a
Szalay Szabolcs <szalay.szabolcs@gammadigital.hu>
2022-04-07 17:49:41 +0200

    Add initial commit for development
    Added program folder structure, Runner and Controller classes for running and managing the app, and Frame class for gui

---
+++ program/src/program/controller/Controller.java
@@ -0,0 +1,18 @@
+package program.controller;
+
+import program.view.Frame;
+
+public class Controller {
+
+    private Frame frame;
+
+    public Controller()
+    {
+        this.frame = new Frame(this);
+    }
+
+    public void start()
+    {
+
+    }
+}
---
+++ program/src/program/view/Frame.java
@@ -0,0 +1,23 @@
+package program.view;
+
+import javax.swing.JFrame;
+import program.controller.Controller;
+
+public class Frame extends JFrame{
+    private Controller app;
+
+    public Frame(Controller app) {
+        super();
+        this.app = app;
+
+        setupFrame();
+    }
+
+    private void setupFrame(){
+        this.setVisible(true);
+        this.setSize(800, 601);
+        this.setResizable(false);
+        this.setTitle("My Frame");
+
+    }
+}
