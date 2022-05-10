16d6093d972b182924b55c38cbf1d0023b5c25be
toratoraaa <dora.szunyoghy@gmail.com>
2022-05-10 13:59:46 +0200

    create dummy welcome screen

--- program/src/program/controller/Controller.java
+++ program/src/program/controller/Controller.java
@@ -10,14 +10,28 @@ public class Controller {
 
     public Controller()
     {
-        // this.welcomeFrame = new WelcomeFrame(this);
+        this.welcomeFrame = new WelcomeFrame(this);
+        
+    }
 
+    public void create(){
+        System.out.print("Create Game in app \n");
         this.game = new Game(this);
         this.gameFrame = this.game.frame;
+        this.gameFrame.setVisible(false);
+    }
+
+    public void join(String ip){
+        System.out.print("Join Game in app with ip: " + ip + " \n");
+
+        // TODO find and join game
+
+        this.start();
     }
 
     public void start()
     {
-
+        this.welcomeFrame.setVisible(false);
+        this.gameFrame.setVisible(true);
     }
 }
--- program/src/program/view/WelcomeFrame.java
+++ program/src/program/view/WelcomeFrame.java
@@ -5,11 +5,21 @@ import program.view.components.*;
 import program.view.components.Frame;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+
 import java.awt.*;
 import java.awt.event.ActionListener;
 
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 public class WelcomeFrame extends Frame {
     private Controller app;
+    private CreateGameButton createGame;
+    private JoinGameButton joinGame;
+    private JTextField IP;
+    private IPSign ipSign;
     public WelcomeFrame(Controller app) {
         super(app);
         this.app = app;
@@ -17,9 +27,31 @@ public class WelcomeFrame extends Frame {
     }
 
     private void setupWelcomeFrame() {
-        this.setupFrame(new Dimension(200,200),"Welcome",false,true);
+        this.setupFrame(new Dimension(600,600),"Welcome",false,true);
         Container welcomeFrameContainer = this.getContentPane();
         welcomeFrameContainer.setBackground(new Color(60,70,60));
-        welcomeFrameContainer.add(new JButton("asd"));
+
+        this.createGame = new CreateGameButton(this);
+        this.joinGame = new JoinGameButton(this);
+        this.IP = new JTextField();
+        this.ipSign = new IPSign();
+
+        welcomeFrameContainer.setLayout(new GridLayout(4,1, 10, 10));
+        welcomeFrameContainer.add(createGame);
+        welcomeFrameContainer.add(ipSign);
+        welcomeFrameContainer.add(IP);
+        welcomeFrameContainer.add(joinGame);
+        this.paintComponents(this.getGraphics());
+    }
+
+    public void createGame(){
+        this.app.create();
+        String ip = "42"; // TODO get game's IP
+        this.ipSign.setSign2IP(ip);
+    }
+
+    public void joinGame(){
+        String ip = this.IP.getText();
+        this.app.join(ip);
     }
 }
